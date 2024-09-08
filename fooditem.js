a191e470b4a1e07e26502de979c88dc0773acf6b
Anhtudoan <21t1020094@husc.edu.vn>
2024-09-08 21:22:25 +0700

    DoanAnhTu

---
+++ fooditem.js
@@ -0,0 +1,331 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'Biryani 1',
+    category : 'biryani',
+    rating : 5,
+    price: 15000,
+    img: 'images/biryani/a1.jpg',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'Biryani 2',
+    category : 'biryani',
+    rating : 5,
+    price: 15.000,
+    img: 'images/biryani/a2.jpg',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Biryani 3',
+    category : 'biryani',
+    rating : 5,
+    price: 18,
+    img: 'images/biryani/a3.jpg',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'Biryani 4',
+    category : 'biryani',
+    rating :5,
+    price: 15,
+    img: 'images/biryani/a4.jpg',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'Biryani 5',
+    category : 'biryani',
+    rating : 5,
+    price: 10,
+    img: 'images/biryani/a5.jpg',
+    quantity: 1
+},
+{
+    id: 6,
+    name: 'Biryani 6',
+    category : 'biryani',
+    rating : 4.3,
+    price: 12,
+    img: 'images/biryani/a6.jpg',
+    quantity: 1
+},
+{
+    id: 10,
+    name: 'Chicken Roast',
+    category : 'chicken',
+    rating : 4.3,
+    price: 11,
+    img: 'images/chicken/a1.jpg',
+    quantity: 1
+},
+{
+    id: 11,
+    name: 'Chicken Curry',
+    category : 'chicken',
+    rating : 4.3,
+    price: 10,
+    img: 'images/chicken/a2.jpg',
+    quantity: 1
+},
+{
+    id: 12,
+    name: 'Chicken Do Pyaza',
+    category : 'chicken',
+    rating : 4.3,
+    price: 14,
+    img: 'images/chicken/a3.jpg',
+    quantity: 1
+},
+{
+    id: 13,
+    name: 'Chicken Masala',
+    category : 'chicken',
+    rating : 4.3,
+    price: 12,
+    img: 'images/chicken/a4.jpg',
+    quantity: 1
+},
+{
+    id: 14,
+    name: 'Handi Chicken',
+    category : 'chicken',
+    rating : 4.3,
+    price: 17,
+    img: 'images/chicken/a5.jpg',
+    quantity: 1
+},
+{
+    id: 15,
+    name: 'Murgh Musallam',
+    category : 'chicken',
+    rating : 4.3,
+    price: 20,
+    img: 'images/chicken/a6.jpg',
+    quantity: 1
+},
+{
+    id: 17,
+    name: 'Palak pizza',
+    category : 'pizza',
+    rating : 4.3,
+    price: 10,
+    img: 'images/pizza/a1.jpg',
+    quantity: 1
+},
+{
+    id: 18,
+    name: 'pizza Butter Masala',
+    category : 'pizza',
+    rating : 4.3,
+    price: 15,
+    img: 'images/pizza/a2.jpg',
+    quantity: 1
+
+},
+{
+    id: 19,
+    name: 'pizza Do Pyaza',
+    category : 'pizza',
+    rating : 4.3,
+    price: 12,
+    img: 'images/pizza/a3.jpg',
+    quantity: 1
+
+},
+{
+    id: 20,
+    name: 'Hyderabadi pizza',
+    category : 'pizza',
+    rating : 4.3,
+    price: 8,
+    img: 'images/pizza/a4.jpg',
+    quantity: 1
+},
+{
+    id: 21,
+    name: 'pizza Lababdar',
+    category : 'pizza',
+    rating : 4.3,
+    price: 7,
+    img: 'images/pizza/a5.jpg',
+    quantity: 1
+},
+{
+    id: 22,
+    name: 'Shahi pizza',
+    age: '32',
+    category : 'pizza',
+    rating : 4.3,
+    price: 5,
+    img: 'images/pizza/a6.jpg',
+    quantity: 1
+},
+{
+    id: 23,
+    name: 'Navratan Korma',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 8,
+    img: 'images/vegetable/navratan-korma_-vegetable.png',
+    quantity: 1
+},
+{
+    id: 24,
+    name: 'Veg Jalfrezi',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 7,
+    img: 'images/vegetable/VEG-JALFREZI.jpg',
+    quantity: 1
+},
+{
+    id: 25,
+    name: 'Veg Biryani',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 5,
+    img: 'images/vegetable/vegetable-biryani.jpg',
+    quantity: 1
+},
+{
+    id: 26,
+    name: 'Veg Curry',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 7,
+    img: 'images/vegetable/vegetable-curry.jpeg',
+    quantity: 1
+},
+{
+    id: 28,
+    name: 'Veg Masala',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 4,
+    img: 'images/vegetable/vegetable-masala.jpg',
+    quantity: 1
+},
+{
+    id: 29,
+    name: 'Veg Pakora',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 4,
+    img: 'images/vegetable/vegetable-pakora.jpg',
+    quantity: 1
+},
+{
+    id: 30,
+    name: 'Momos',
+    category : 'chinese',
+    rating : 4.3,
+    price: 8,
+    img: 'images/chinese/a1.jpg',
+    quantity: 1
+},
+{
+    id: 31,
+    name: 'Chicken Manchurian',
+    category : 'chinese',
+    rating : 4.3,
+    price: 7,
+    img: 'images/chinese/a2.jpg',
+    quantity: 1
+},
+{
+    id: 32,
+    name: 'Chili Chicken',
+    category : 'chinese',
+    rating : 4.3,
+    price: 5,
+    img: 'images/chinese/a3.jpg',
+    quantity: 1
+},
+{
+    id: 34,
+    name: 'Spring Roll',
+    category : 'chinese',
+    rating : 4.3,
+    price: 14,
+    img: 'images/chinese/a4.jpg',
+    quantity: 1
+},
+{
+    id: 35,
+    name: 'Szechuan Chicken',
+    category : 'chinese',
+    rating : 4.3,
+    price: 10,
+    img: 'images/chinese/a5.jpg',
+    quantity: 1
+},
+{
+    id: 36,
+    name: 'Fried Rice',
+    category : 'chinese',
+    rating : 4.3,
+    price: 8,
+    img: 'images/chinese/a6.jpg',
+    quantity: 1
+},
+{
+    id: 37,
+    name: 'Butter Masala Dosa',
+    category : 'drinkis',
+    rating : 4.3,
+    price: 18,
+    img: 'images/drinks/a1.jpg',
+    quantity: 1
+},
+{
+    id: 38,
+    name: 'Idli',
+    category : 'drinkis',
+    rating : 4.3,
+    price: 20,
+    img: 'images/drinks/a2.jpg',
+    quantity: 1
+},
+{
+    id: 39,
+    name: 'Masala Dosa',
+    category : 'drinkis',
+    rating : 4.3,
+    price: 12,
+    img: 'images/drinks/a3.jpg',
+    quantity: 1
+},
+{
+    id: 40,
+    name: 'Mysore Bonda',
+    category : 'drinkis',
+    rating : 4.3,
+    price: 10,
+    img: 'images/drinks/a4.jpg',
+    quantity: 1
+},
+{
+    id: 41,
+    name: 'Onion Uttapam',
+    category : 'drinkis',
+    rating : 4.3,
+    price: 15,
+    img: 'images/drinks/a5.jpg',
+    quantity: 1
+},
+{
+    id: 42,
+    name: 'Plain Dosa 1',
+    category : 'drinkis',
+    rating : 4.3,
+    price: 40,
+    img: 'images/drinks/a6.jpg',
+    quantity: 1
+},
+]
+
+export {foodItem};
